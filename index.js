5ec2ce23e832b4c113682eb1ed930e26b7c17378
Nikita <terencekity@gmail.com>
2023-08-20 07:28:32 -0400

    complete the lab

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout (str){
+   return str.toUpperCase()
+}
+function whisper (str){
+    return str.toLowerCase()
+}
+function logShout(str){
+    console.log(str.toUpperCase())
+}
+function logWhisper(str){
+    console.log(str.toLowerCase())
+}
+function sayHiToHeadphonedRoommate(str){
+
+}
+function sayHiToHeadphonedRoommate(str){
+    if (str === "Let's have dinner together!"){return  "I would love to!"}
+    else if (str === str.toLowerCase()){return "I can't hear you!"}
+    else return  "YES INDEED!"
+}
